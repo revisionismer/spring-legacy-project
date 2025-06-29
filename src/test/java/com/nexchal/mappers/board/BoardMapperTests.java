184639179a3940dca1712fcae2b6669d21f14c70
revisionismer <cherry2137@naver.com>
2025-06-29 10:01:01 +0900

    6. Mybatis를 이용한 CRUD와 서비스 계층의 설계와 구현

---
+++ src/test/java/com/nexchal/mappers/board/BoardMapperTests.java
@@ -0,0 +1,67 @@
+package com.nexchal.mappers.board;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.nexchal.board.domain.BoardVO;
+import com.nexchal.board.mappers.board.BoardMapper;
+
+import lombok.extern.log4j.Log4j2;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j2
+public class BoardMapperTests {
+
+	@Autowired(required = false)
+	BoardMapper boardMapper;
+	
+	@Test
+	public void test01() {
+		log.info(boardMapper);
+	}
+	
+	@Test
+	public void test02() {
+		boardMapper.getAllList().forEach(boardVO -> log.info(boardVO));
+	}
+	
+	@Test
+	public void test03() {
+		
+		BoardVO boardVO = new BoardVO();
+		boardVO.setTitle("New Test Title");
+		boardVO.setContent("New Test Content");
+		boardVO.setWriter("New Test Writer");
+		
+		log.info("COUNT : " + boardMapper.insert(boardVO));
+		
+		log.info("BNO : " + boardVO.getBno());
+	}
+	
+	
+	@Test
+	public void test04() {
+		
+		Long bno = 3L;
+		
+		log.info(boardMapper.select(bno));
+	}
+	
+	@Test
+	public void test05() {
+		
+		Long bno = 2L;
+		
+		BoardVO findBoard = boardMapper.select(bno);
+		findBoard.setTitle("New Test Title(수정)");
+		findBoard.setContent("New Test Content(수정)");
+		
+		int updateCount = boardMapper.update(findBoard);
+		
+		log.info("update count : " + updateCount);
+	}
+}
--- src/test/java/com/nexchal/service/board/BoardServiceTests.java
+++ src/test/java/com/nexchal/service/board/BoardServiceTests.java
@@ -1,4 +1,4 @@
-package com.nexchal.mappers;
+package com.nexchal.service.board;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,25 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.nexchal.board.mappers.board.BoardMapper;
+import com.nexchal.board.service.board.BoardServiceImpl;
 
 import lombok.extern.log4j.Log4j2;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j2
-public class BoardMapperTests {
+public class BoardServiceTests {
 
-	@Autowired(required = false)
-	BoardMapper boardMapper;
-	
+	@Autowired
+	BoardServiceImpl boardService;
+
+	// 여기까지
 	@Test
 	public void test01() {
-		log.info(boardMapper);
-	}
-	
-	@Test
-	public void test02() {
-		boardMapper.getAllList().forEach(boardVO -> log.info(boardVO));
+		log.info(boardService.readBoardlist());
 	}
 }
