d412582bc46d634f2e4915113fa603797026fcc0
revisionismer <cherry2137@naver.com>
2025-07-05 21:47:52 +0900

    8. 조회와 입력 화면은 GET 방식, 실제 처리는 POST 방식

--- src/main/java/com/nexchal/board/web/board/BoardController.java
+++ src/main/java/com/nexchal/board/web/board/BoardController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.nexchal.board.domain.BoardVO;
 import com.nexchal.board.service.board.BoardServiceImpl;
@@ -20,8 +23,8 @@ import lombok.extern.log4j.Log4j2;
 public class BoardController {
 	
 	private final BoardServiceImpl boardService;
-
-	// 1-1. list
+	
+	// 1-1. getAllBoardlist 
 	@GetMapping("/list")
 	public void getAllBoardlist(Model model) {
 		log.info("------------list----------");
@@ -33,4 +36,52 @@ public class BoardController {
 		log.info(result);
 	}
 	
+	// 1-2. readBoardOne
+	@GetMapping("/read/{bno}")
+	public String getBoardOne(@PathVariable(name = "bno") Long bno, Model model) {
+		log.info("------------boardOne----------");
+		
+		BoardVO boardVO = boardService.readBoardOne(bno);
+		
+		log.info("boardVO : " + boardVO);
+		
+		model.addAttribute("board", boardVO);
+		
+		return "/board/read";
+	}
+	
+	// 1-3. modify
+	@GetMapping("/modify/{bno}")
+	public String modifyBoardOne(@PathVariable(name = "bno") Long bno, Model model) {
+		log.info("------------boardOne----------");
+		
+		BoardVO boardVO = boardService.readBoardOne(bno);
+		
+		log.info("boardVO : " + boardVO);
+		
+		model.addAttribute("board", boardVO);
+		
+		return "/board/modify";
+	}
+	
+
+	// 1-4. register(화면)
+	@GetMapping("/register")
+	public String registerBoardGET(Model model) {
+		log.info("------------registerBoard 화면 이동----------");
+		
+		return "/board/register";
+	}
+
+	// 1-5. register(등록)
+	@PostMapping("/register")
+	public String registerBoardPOST(BoardVO boardVO, RedirectAttributes rttr) {
+		log.info("------------board write ----------");
+		
+		Long bno = boardService.createBoard(boardVO);
+	
+		rttr.addFlashAttribute("result", bno);
+		
+		return "redirect:/board/list";
+	}
 }
